21b138320d6b13bcb81fb16aa6d6e6720719dc96
Paul Anthony McGowan <xhostcom@gmail.com>
2021-08-25 13:25:26 +0100

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,20 @@ authenticate :user, lambda { |u| u.admin? } do
   mount Sidekiq::Web => '/sidekiq'
 
   namespace :madmin do
+    resources :services
+    resources :announcements
+    resources :notifications
+    namespace :active_storage do
+      resources :variant_records
+    end
+    namespace :active_storage do
+      resources :attachments
+    end
+    namespace :active_storage do
+      resources :blobs
+    end
+    resources :users
+    root to: "dashboard#show"
   end
 end
 
